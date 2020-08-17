18ad4115e6f54a461333474fadc34d80a2f70d59
C. Serrano <serrano.cjm@gmail.com>
2020-08-16 22:39:26 -0400

    Add dependencies for development

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("android")
     kotlin("android.extensions")
     id("kotlin-android")
+    id("io.objectbox")
 }
 
 android {
@@ -36,12 +37,40 @@ android {
 }
 
 dependencies {
-    //implementation(fileTree(dir: "libs", include: ["*.jar"]))
+    //Core dependencies
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.72")
     implementation("androidx.core:core-ktx:1.3.1")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.3.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.3.0")
+}
+
+dependencies {
+    //Test dependencies
     testImplementation("junit:junit:4.12")
     androidTestImplementation("androidx.test.ext:junit:1.1.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+}
+
+dependencies {
+    //Third-party dependencies
+
+    //OKHTTP
+    implementation("com.squareup.okhttp3:okhttp:4.0.1")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.0.1")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.7.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.7.0")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2")
+
+    //threeten
+    implementation("com.jakewharton.threetenabp:threetenabp:1.2.4")
+
+    //GLIDE
+    implementation("com.github.bumptech.glide:glide:4.9.0")
+    kapt("com.github.bumptech.glide:compiler:4.9.0")
+
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val kotlin_version by extra("1.3.72")
+    val kotlinVersion by extra("1.3.72")
+    val objectboxVersion by extra("2.7.0")
+
     repositories {
         google()
         jcenter()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.0.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("io.objectbox:objectbox-gradle-plugin:$objectboxVersion")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
