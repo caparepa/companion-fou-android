067738a1bd25f3b71cf0f7cf5e27954cbcfcf859
C. Serrano <serrano.cjm@gmail.com>
2020-08-22 13:34:47 -0400

    craft essence & MaterialItem

---
+++ app/src/main/java/com/caparepa/companionfou/data/model/nice/materialitem/MaterialItem.kt
@@ -0,0 +1,17 @@
+package com.caparepa.companionfou.data.model.nice.materialitem
+
+
+import com.google.gson.annotations.SerializedName
+
+data class MaterialItem(
+    @SerializedName("id")
+    val id: Int? = null,
+    @SerializedName("name")
+    val name: String? = null,
+    @SerializedName("type")
+    val type: String? = null,
+    @SerializedName("icon")
+    val icon: String? = null,
+    @SerializedName("background")
+    val background: String? = null
+)
