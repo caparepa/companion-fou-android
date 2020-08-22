404664f50b903cd861815bfea519dc101c4454bb
C. Serrano <serrano.cjm@gmail.com>
2020-08-21 23:41:10 -0400

    control commit

--- app/src/main/java/com/caparepa/companionfou/data/model/en/nice/commandcode/CommandCodeList.kt
+++ app/src/main/java/com/caparepa/companionfou/data/model/en/nice/commandcode/CommandCodeList.kt
@@ -3,5 +3,4 @@ package com.caparepa.companionfou.data.model.en.nice.commandcode
 
 import com.google.gson.annotations.SerializedName
 
-class CommandCodeList : ArrayList<CommandCodeItem>(){
-}
+class CommandCodeList : ArrayList<CommandCodeItem>()
