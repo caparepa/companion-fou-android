5beede77c9d2d98ec3781c825e0a317127c46421
C. Serrano <serrano.cjm@gmail.com>
2020-08-30 21:21:57 -0400

    setup app

--- app/src/main/java/com/caparepa/companionfou/CompanionFouApplication.kt
+++ app/src/main/java/com/caparepa/companionfou/CompanionFouApplication.kt
@@ -3,13 +3,24 @@ package com.caparepa.companionfou
 import android.app.Application
 import com.caparepa.companionfou.di.CoreModule
 import com.jakewharton.threetenabp.AndroidThreeTen
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
 class CompanionFouApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        //initKoin()
         //CoreModule.init()
         //AndroidThreeTen.init(this)
+    }
+
+    private fun initKoin() {
+        startKoin {
+            androidLogger()
+            androidContext(this@CompanionFouApplication)
+        }
     }
 
 }
