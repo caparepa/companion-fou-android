c8b14afe838aae8d377516dbb7f6660643e053ee
C. Serrano <serrano.cjm@gmail.com>
2020-08-30 20:54:24 -0400

    Add interceptor and exceptions

--- app/src/main/java/com/caparepa/companionfou/network/interceptor/RequestInterceptor.kt
+++ app/src/main/java/com/caparepa/companionfou/network/interceptor/RequestInterceptor.kt
@@ -2,5 +2,5 @@ package com.caparepa.companionfou.network.interceptor
 
 import okhttp3.Interceptor
 
-interface ConnectivityInterceptor: Interceptor {
+interface RequestInterceptor: Interceptor {
 }
--- app/src/main/java/com/caparepa/companionfou/utils/Exceptions.kt
+++ app/src/main/java/com/caparepa/companionfou/utils/Exceptions.kt
@@ -1,2 +1,11 @@
 package com.caparepa.companionfou.utils
 
+import java.io.IOException
+
+class NoConnectivityException : IOException()
+
+class LocationPermissionNotGrantedException : Exception()
+
+class DateNotFoundException : Exception()
+
+class ApiException(message: String) : IOException(message)
