65fb2e244f84cbaf1108422552942996b30a1a3f
C. Serrano <serrano.cjm@gmail.com>
2020-08-30 21:19:51 -0400

    Add di modules

--- app/src/main/java/com/caparepa/companionfou/di/CoreModule.kt
+++ app/src/main/java/com/caparepa/companionfou/di/CoreModule.kt
@@ -1,4 +1,12 @@
 package com.caparepa.companionfou.di
 
+import com.caparepa.companionfou.di.modules.databaseModule
+import com.caparepa.companionfou.di.modules.localModule
+import com.caparepa.companionfou.di.modules.networkModule
+import com.caparepa.companionfou.di.modules.repositoryModule
+import org.koin.core.context.loadKoinModules
+
 object CoreModule {
+    private val modules = listOf(networkModule, repositoryModule, localModule, databaseModule)
+    fun init() = loadKoinModules(modules)
 }
--- app/src/main/java/com/caparepa/companionfou/di/modules/Modules.kt
+++ app/src/main/java/com/caparepa/companionfou/di/modules/Modules.kt
@@ -1,2 +1,23 @@
 package com.caparepa.companionfou.di.modules
 
+import com.caparepa.companionfou.network.api.AtlasApiClient
+import com.caparepa.companionfou.network.interceptor.ConnectivityInterceptor
+import com.caparepa.companionfou.network.interceptor.ConnectivityInterceptorImpl
+import org.koin.dsl.module
+
+val networkModule = module {
+    single { AtlasApiClient.invoke() }
+    single<ConnectivityInterceptor> { ConnectivityInterceptorImpl(get()) }
+}
+
+val repositoryModule = module {
+
+}
+
+val localModule = module {
+    //Shared preferences here!
+}
+
+val databaseModule = module {
+
+}
