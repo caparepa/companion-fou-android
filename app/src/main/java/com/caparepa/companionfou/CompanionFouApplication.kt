dc6638acf30b3d6cfce064b751cf1ec4fafd1140
C. Serrano <serrano.cjm@gmail.com>
2020-08-30 21:21:07 -0400

    setup app

--- app/src/main/java/com/caparepa/companionfou/CompanionFouApplication.kt
+++ app/src/main/java/com/caparepa/companionfou/CompanionFouApplication.kt
@@ -1,4 +1,15 @@
 package com.caparepa.companionfou
 
-class CompanionFouApplication {
+import android.app.Application
+import com.caparepa.companionfou.di.CoreModule
+import com.jakewharton.threetenabp.AndroidThreeTen
+
+class CompanionFouApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        //CoreModule.init()
+        //AndroidThreeTen.init(this)
+    }
+
 }
