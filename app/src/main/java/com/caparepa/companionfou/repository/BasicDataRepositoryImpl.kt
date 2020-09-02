d218d9d37262035c3f388e238a3b6c98c76c096b
C. Serrano <serrano.cjm@gmail.com>
2020-09-02 16:09:14 -0400

    Implement different calls

--- app/src/main/java/com/caparepa/companionfou/repository/BasicDataRepositoryImpl.kt
+++ app/src/main/java/com/caparepa/companionfou/repository/BasicDataRepositoryImpl.kt
@@ -25,28 +25,40 @@ class BasicDataRepositoryImpl : BasicDataRepository, KoinComponent {
     override suspend fun getBasicServantsEnglishName(
         urlType: String,
         region: String
-    ): List<ServantItem>? = withContext(Dispatchers.Main) {
-        null
+    ): List<ServantItem>? = withContext(Dispatchers.IO) {
+        val response = api.getBasicServantsEnglishName(urlType, region)
+        val bodyString = response?.body()?.string()
+        val data = bodyString?.toKotlinObject<List<ServantItem>>()
+        data
     }
 
     override suspend fun getBasicCraftEssences(
         urlType: String,
         region: String
-    ): List<CraftEssenceItem>? = withContext(Dispatchers.Main) {
-        null
+    ): List<CraftEssenceItem>? = withContext(Dispatchers.IO) {
+        val response = api.getBasicCraftEssences(urlType, region)
+        val bodyString = response?.body()?.string()
+        val data = bodyString?.toKotlinObject<List<CraftEssenceItem>>()
+        data
     }
 
     override suspend fun getBasicCommandCodes(
         urlType: String,
         region: String
-    ): List<CommandCodeItem>? = withContext(Dispatchers.Main) {
-        null
+    ): List<CommandCodeItem>? = withContext(Dispatchers.IO) {
+        val response = api.getBasicCommandCodes(urlType, region)
+        val bodyString = response?.body()?.string()
+        val data = bodyString?.toKotlinObject<List<CommandCodeItem>>()
+        data
     }
 
     override suspend fun getBasicMysticCodes(
         urlType: String,
         region: String
-    ): List<MysticCodeItem>? = withContext(Dispatchers.Main) {
-        null
+    ): List<MysticCodeItem>? = withContext(Dispatchers.IO) {
+        val response = api.getBasicCommandCodes(urlType, region)
+        val bodyString = response?.body()?.string()
+        val data = bodyString?.toKotlinObject<List<MysticCodeItem>>()
+        data
     }
 }
