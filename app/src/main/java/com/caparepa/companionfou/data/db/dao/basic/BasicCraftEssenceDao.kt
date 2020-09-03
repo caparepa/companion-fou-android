4e814d1f824e1da4999cadc36095a83a06d81759
C. Serrano <serrano.cjm@gmail.com>
2020-09-03 01:02:50 -0400

    dao (WIP)

--- app/src/main/java/com/caparepa/companionfou/data/db/dao/basic/BasicCraftEssenceDao.kt
+++ app/src/main/java/com/caparepa/companionfou/data/db/dao/basic/BasicCraftEssenceDao.kt
@@ -1,4 +1,34 @@
 package com.caparepa.companionfou.data.db.dao.basic
 
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.caparepa.companionfou.data.db.entity.basic.BasicCraftEssenceEntity
+
 interface BasicCraftEssenceDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(ce: BasicCraftEssenceEntity)
+
+    @Query("SELECT * FROM basic_craft_essence")
+    suspend fun getCraftEssences(): List<BasicCraftEssenceEntity>
+
+    @Query("SELECT * FROM basic_craft_essence WHERE _id = :id")
+    suspend fun getCraftEssenceById(id: Long): BasicCraftEssenceEntity
+
+    @Query("SELECT * FROM basic_craft_essence WHERE collectionNo = :collectionNo")
+    suspend fun getCraftEssenceByCollectionNo(collectionNo: Long): BasicCraftEssenceEntity
+
+    @Query("SELECT * FROM basic_craft_essence WHERE type = :ceType")
+    suspend fun getCraftEssenceByType(ceType: String): List<BasicCraftEssenceEntity>
+
+    @Query("SELECT * FROM basic_craft_essence WHERE name LIKE :name")
+    suspend fun getCraftEssenceByName(name: String): List<BasicCraftEssenceEntity>
+
+    @Query("SELECT * FROM basic_craft_essence WHERE className = :className")
+    suspend fun getCraftEssenceByClassName(className: String): List<BasicCraftEssenceEntity>
+
+    @Query("SELECT * FROM basic_craft_essence WHERE rarity = :rarity")
+    suspend fun getCraftEssenceByRarity(rarity: Int): List<BasicCraftEssenceEntity>
+
 }
