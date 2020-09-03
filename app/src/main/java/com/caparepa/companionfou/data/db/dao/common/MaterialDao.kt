b1018cc3634df2623dd0c080b8025bbe97ed3ece
C. Serrano <serrano.cjm@gmail.com>
2020-09-02 23:59:58 -0400

    control commit

--- app/src/main/java/com/caparepa/companionfou/data/db/dao/common/MaterialDao.kt
+++ app/src/main/java/com/caparepa/companionfou/data/db/dao/common/MaterialDao.kt
@@ -1,4 +1,22 @@
 package com.caparepa.companionfou.data.db.dao.common
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.caparepa.companionfou.data.db.entity.common.MaterialItem
+
+@Dao
 interface MaterialDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(materialEntry: MaterialItem)
+
+    @Query("SELECT * FROM material_item")
+    suspend fun getAllMaterials(): List<MaterialItem>
+
+    @Query("SELECT * FROM material_item WHERE _id = :id")
+    suspend fun getMaterial(id: Long): MaterialItem
+
+
 }
