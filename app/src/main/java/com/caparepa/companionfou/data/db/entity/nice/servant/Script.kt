a6b0fe7e98e5e0d61dffee997c149ea040d50fcd
C. Serrano <serrano.cjm@gmail.com>
2020-09-09 01:26:16 -0400

    control commit

--- app/src/main/java/com/caparepa/companionfou/data/db/entity/nice/servant/Script.kt
+++ app/src/main/java/com/caparepa/companionfou/data/db/entity/nice/servant/Script.kt
@@ -1,4 +1,10 @@
 package com.caparepa.companionfou.data.db.entity.nice.servant
 
-class Script(
-)
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "skill_script")
+class Script() {
+    @PrimaryKey(autoGenerate = true)
+    var mId: Long = 0
+}
