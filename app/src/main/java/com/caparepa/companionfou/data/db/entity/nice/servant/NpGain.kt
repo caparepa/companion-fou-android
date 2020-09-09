15f51fd2bb62908b2ad662f754c9b526d01db618
C. Serrano <serrano.cjm@gmail.com>
2020-09-09 01:23:09 -0400

    control commit

--- app/src/main/java/com/caparepa/companionfou/data/db/entity/nice/servant/NpGain.kt
+++ app/src/main/java/com/caparepa/companionfou/data/db/entity/nice/servant/NpGain.kt
@@ -1,7 +1,12 @@
 package com.caparepa.companionfou.data.db.entity.nice.servant
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity(
+    tableName = "np_gain"
+)
 data class NpGain(
     @SerializedName("buster")
     val buster: List<Int?>? = null,
@@ -15,4 +20,7 @@ data class NpGain(
     val defence: List<Int?>? = null,
     @SerializedName("np")
     val np: List<Int?>? = null
-)
+) {
+    @PrimaryKey(autoGenerate = true)
+    var mId: Long = 0
+}
