20a09ae1e776487448908ab85076360b15daef95
C. Serrano <serrano.cjm@gmail.com>
2020-09-11 23:01:09 -0400

    control commit

--- app/src/main/java/com/caparepa/companionfou/data/model/nice/servant/NiceServantItem.kt
+++ app/src/main/java/com/caparepa/companionfou/data/model/nice/servant/NiceServantItem.kt
@@ -2,7 +2,7 @@ package com.caparepa.companionfou.data.model.nice.servant
 
 import com.google.gson.annotations.SerializedName
 
-data class ServantItem(
+data class NiceServantItem(
     @SerializedName("id")
     val id: Long? = null,
     @SerializedName("collectionNo")
