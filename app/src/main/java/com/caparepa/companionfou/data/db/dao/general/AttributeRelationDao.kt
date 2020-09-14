1a85800193a2113d24a3f36a54b7c0ce5d847db9
C. Serrano <serrano.cjm@gmail.com>
2020-09-14 19:19:40 -0400

    Dao

---
+++ app/src/main/java/com/caparepa/companionfou/data/db/dao/general/AttributeRelationDao.kt
@@ -0,0 +1,16 @@
+package com.caparepa.companionfou.data.db.dao.general
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.caparepa.companionfou.data.db.entity.general.AttributeRelationEntity
+
+@Dao
+interface AttributeRelationDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(item: AttributeRelationEntity)
+
+    @Query("SELECT * FROM servant WHERE id = :id")
+    suspend fun getAttributeRelationData(id: Long): List<AttributeRelationEntity>
+}
