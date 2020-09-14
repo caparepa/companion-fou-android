cec8b6a471e8f14236f47242bb0934a3b77386aa
C. Serrano <serrano.cjm@gmail.com>
2020-09-14 19:14:41 -0400

    dao

--- app/src/main/java/com/caparepa/companionfou/data/db/dao/nice/CraftEssenceDao.kt
+++ app/src/main/java/com/caparepa/companionfou/data/db/dao/nice/CraftEssenceDao.kt
@@ -1,4 +1,19 @@
 package com.caparepa.companionfou.data.db.dao.nice
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.caparepa.companionfou.data.db.entity.nice.CraftEssenceEntity
+
+@Dao
 interface CraftEssenceDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(craftEssence: CraftEssenceEntity)
+
+    @Query("SELECT * FROM craft_essence")
+    suspend fun getCraftEssences(): List<CraftEssenceEntity>
+
+    @Query("SELECT * FROM craft_essence WHERE id = :id")
+    suspend fun getCraftEssenceById(id: Long): CraftEssenceEntity
 }
--- app/src/main/java/com/caparepa/companionfou/data/db/dao/nice/MaterialDao.kt
+++ app/src/main/java/com/caparepa/companionfou/data/db/dao/nice/MaterialDao.kt
@@ -1,4 +1,19 @@
 package com.caparepa.companionfou.data.db.dao.nice
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.caparepa.companionfou.data.db.entity.nice.MaterialEntity
+
+@Dao
 interface MaterialDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(material: MaterialEntity)
+
+    @Query("SELECT * FROM material")
+    suspend fun getMaterials(): List<MaterialEntity>
+
+    @Query("SELECT * FROM material WHERE id = :id")
+    suspend fun getMaterialById(id: Long): MaterialEntity
 }
