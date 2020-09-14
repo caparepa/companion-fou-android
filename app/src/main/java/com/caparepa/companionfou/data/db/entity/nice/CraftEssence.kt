455da87dd1072e7f5a1409654c8ee06476cc3245
C. Serrano <serrano.cjm@gmail.com>
2020-09-14 08:45:16 -0400

    craft essence entity

--- app/src/main/java/com/caparepa/companionfou/data/db/entity/nice/CraftEssence.kt
+++ app/src/main/java/com/caparepa/companionfou/data/db/entity/nice/CraftEssence.kt
@@ -3,6 +3,9 @@ package com.caparepa.companionfou.data.db.entity.nice
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import com.caparepa.companionfou.data.model.nice.common.ExtraAssets
+import com.caparepa.companionfou.data.model.nice.common.SkillItem
+import com.caparepa.companionfou.utils.toKotlinObject
 import com.google.gson.JsonArray
 import com.google.gson.JsonElement
 import com.google.gson.annotations.SerializedName
@@ -23,16 +26,32 @@ data class CraftEssence(
     val cost: Int? = null,
     val lv_max: Int? = null,
     /** ExtraAssets? **/
-    val extra_assets: JsonElement? = null,
+    val extra_assets: String? = null,
     val atk_base: Int? = null,
     val atk_max: Int? = null,
     val hp_base: Int? = null,
     val hp_max: Int? = null,
     val growth_curve: Int? = null,
     /** List<Int?>? **/
-    val atk_growth: JsonArray? = null,
+    val atk_growth: String? = null,
     /** List<Int?>? **/
-    val hp_growth: JsonArray? = null,
+    val hp_growth: String? = null,
     /** List<SkillItem?>? **/
-    val skills: JsonArray? = null
-)
+    val skills: String? = null
+) {
+    fun getExtraAssets(): ExtraAssets? {
+        return this.extra_assets?.toKotlinObject()
+    }
+
+    fun getAtkGrowth(): List<Int>? {
+        return this.atk_growth?.toKotlinObject()
+    }
+
+    fun getHpGrowth(): List<Int>? {
+        return this.hp_growth?.toKotlinObject()
+    }
+
+    fun getSkills(): List<SkillItem>? {
+        return this.skills?.toKotlinObject()
+    }
+}
