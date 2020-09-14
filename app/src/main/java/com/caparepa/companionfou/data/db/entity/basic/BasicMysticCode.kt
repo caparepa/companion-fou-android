dd1e8a42dd8ff95c61fc62f0cadf06ea30da9a8b
C. Serrano <serrano.cjm@gmail.com>
2020-09-14 18:08:26 -0400

    control commit

--- app/src/main/java/com/caparepa/companionfou/data/db/entity/basic/BasicMysticCode.kt
+++ app/src/main/java/com/caparepa/companionfou/data/db/entity/basic/BasicMysticCode.kt
@@ -3,14 +3,19 @@ package com.caparepa.companionfou.data.db.entity.basic
 import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.caparepa.companionfou.utils.toKotlinObject
 
 @Entity(tableName = "basic_mystic_code")
 data class BasicMysticCode(
     val id: Long? = null,
     val name: String? = null,
     @Embedded(prefix = "media_")
-    val item: BasicMysticCodeMedia? = null
+    val item: String? = null
 ) {
     @PrimaryKey(autoGenerate = true)
     var mCodeId: Long = 0
+
+    fun getBasicMysticCodeMedia(): BasicMysticCodeMedia? {
+        return this.item?.toKotlinObject()
+    }
 }
