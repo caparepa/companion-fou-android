c4976dd784e7d7fa9bacd7fa3c92017f366d49b9
C. Serrano <serrano.cjm@gmail.com>
2020-09-14 19:15:57 -0400

    dao

--- app/src/main/java/com/caparepa/companionfou/data/db/dao/nice/CommandCodeDao.kt
+++ app/src/main/java/com/caparepa/companionfou/data/db/dao/nice/CommandCodeDao.kt
@@ -1,4 +1,19 @@
 package com.caparepa.companionfou.data.db.dao.nice
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.caparepa.companionfou.data.db.entity.nice.CommandCodeEntity
+
+@Dao
 interface CommandCodeDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(commandCode: CommandCodeEntity)
+
+    @Query("SELECT * FROM command_code")
+    suspend fun getCommandCodes(): List<CommandCodeEntity>
+
+    @Query("SELECT * FROM command_code WHERE id = :id")
+    suspend fun getCommandCodeById(id: Long): CommandCodeEntity
 }
