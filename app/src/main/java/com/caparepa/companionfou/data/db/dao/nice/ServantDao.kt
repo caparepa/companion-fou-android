f79ead183b75183cce8c93c323baa5f44acb1337
C. Serrano <serrano.cjm@gmail.com>
2020-09-14 19:10:00 -0400

    control commit

--- app/src/main/java/com/caparepa/companionfou/data/db/dao/nice/ServantDao.kt
+++ app/src/main/java/com/caparepa/companionfou/data/db/dao/nice/ServantDao.kt
@@ -4,35 +4,35 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.caparepa.companionfou.data.model.nice.servant.ServantItem
+import com.caparepa.companionfou.data.db.entity.nice.ServantEntity
 
 @Dao
 interface ServantDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun upsert(servant: ServantItem)
+    suspend fun upsert(servant: ServantEntity)
 
     @Query("SELECT * FROM servant")
-    suspend fun getServants(): List<ServantItem>
+    suspend fun getServants(): List<ServantEntity>
 
     @Query("SELECT * FROM servant WHERE id = :id")
-    suspend fun getServantById(id: Long): ServantItem
+    suspend fun getServantById(id: Long): ServantEntity
 
     @Query("SELECT * FROM servant WHERE collection_no = :collectionNo")
-    suspend fun getServantByCollectionNo(collectionNo: Long): ServantItem
+    suspend fun getServantByCollectionNo(collectionNo: Long): ServantEntity
 
     @Query("SELECT * FROM servant WHERE class_name = :className")
-    suspend fun getServantByClass(className: String): List<ServantItem>
+    suspend fun getServantByClass(className: String): List<ServantEntity>
 
     @Query("SELECT * FROM servant WHERE rarity = :rarity")
-    suspend fun getServantByRarity(rarity: Int): List<ServantItem>
+    suspend fun getServantByRarity(rarity: Int): List<ServantEntity>
 
     @Query("SELECT * FROM servant WHERE type = :servantType")
-    suspend fun getServantByType(servantType: String): List<ServantItem>
+    suspend fun getServantByType(servantType: String): List<ServantEntity>
 
     @Query("SELECT * FROM servant WHERE class_name = :className OR rarity = :rarity OR type = :servantType")
     suspend fun getServantByFilter(
         className: String,
         rarity: Int,
         servantType: String
-    ): List<ServantItem>
+    ): List<ServantEntity>
 }
