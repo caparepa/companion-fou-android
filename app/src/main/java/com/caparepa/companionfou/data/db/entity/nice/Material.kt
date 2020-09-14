0839456eba72b09e7d603fcd8f6bb244775a0721
C. Serrano <serrano.cjm@gmail.com>
2020-09-13 23:58:45 -0400

    material entity

---
+++ app/src/main/java/com/caparepa/companionfou/data/db/entity/nice/Material.kt
@@ -0,0 +1,16 @@
+package com.caparepa.companionfou.data.db.entity.nice
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "material"
+)
+data class Material(
+    @PrimaryKey
+    val id: Long? = null,
+    val name: String? = null,
+    val type: String? = null,
+    val icon: String? = null,
+    val background: String? = null
+)
