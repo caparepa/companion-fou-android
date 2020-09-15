f09c2706556cbb4bf84cb7fd4521f83c6b78d791
C. Serrano <serrano.cjm@gmail.com>
2020-09-15 12:57:50 -0400

    control commit

--- app/src/main/java/com/caparepa/companionfou/data/db/dao/general/AllTraitsDao.kt
+++ app/src/main/java/com/caparepa/companionfou/data/db/dao/general/AllTraitsDao.kt
@@ -12,9 +12,6 @@ interface AllTraitsDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun upsert(item: AllTraitsEntity)
 
-    @Query("SELECT * FROM all_traits")
-    suspend fun getAllTraits(): List<String>?
-
-    @Query("SELECT * FROM all_traits WHERE id = :id")
+    @Query("SELECT * FROM all_traits WHERE id = :0")
     suspend fun getTraitById(id: Long): String?
 }
