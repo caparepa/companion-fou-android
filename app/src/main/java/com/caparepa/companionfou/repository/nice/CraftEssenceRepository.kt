5a1c15f9bad488aa4cee0805a0a3353850825851
C. Serrano <serrano.cjm@gmail.com>
2020-09-15 22:44:24 -0400

    control commit

--- app/src/main/java/com/caparepa/companionfou/repository/nice/CraftEssenceRepository.kt
+++ app/src/main/java/com/caparepa/companionfou/repository/nice/CraftEssenceRepository.kt
@@ -4,7 +4,7 @@ import com.caparepa.companionfou.data.db.entity.nice.CraftEssenceEntity
 import com.caparepa.companionfou.data.model.nice.craftessence.CraftEssenceItem
 
 interface CraftEssenceRepository {
-    suspend fun fetchCommandCode(id: Long): CraftEssenceEntity?
+    suspend fun fetchCraftEssence(id: Long): CraftEssenceEntity?
     suspend fun fetchCraftEssences(): List<CraftEssenceEntity>?
     suspend fun getCraftEssenceList(currentDate: String, region: String): List<CraftEssenceItem>?
 }
