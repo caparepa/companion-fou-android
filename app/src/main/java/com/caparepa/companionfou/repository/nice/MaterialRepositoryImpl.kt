cafe65d8f9c0258377ef7a88b80e64a84b7e67b0
C. Serrano <serrano.cjm@gmail.com>
2020-09-15 22:59:41 -0400

    material repository

--- app/src/main/java/com/caparepa/companionfou/repository/nice/MaterialRepositoryImpl.kt
+++ app/src/main/java/com/caparepa/companionfou/repository/nice/MaterialRepositoryImpl.kt
@@ -4,6 +4,7 @@ import com.caparepa.companionfou.data.db.dao.nice.MaterialDao
 import com.caparepa.companionfou.data.db.entity.nice.MaterialEntity
 import com.caparepa.companionfou.data.model.nice.material.MaterialItem
 import com.caparepa.companionfou.network.api.ApiClient
+import com.caparepa.companionfou.utils.mapTo
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.koin.core.KoinComponent
@@ -35,6 +36,11 @@ class MaterialRepositoryImpl(private val materialDao: MaterialDao) : MaterialRep
         }
 
     private suspend fun persistMaterialList(mList: List<MaterialItem>?) {
-
+        mList?.let {
+            it.forEach { item ->
+                val entry = item.mapTo(MaterialEntity::class.java)
+                materialDao.upsert(entry!!)
+            }
+        }
     }
 }
