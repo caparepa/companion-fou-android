e5b43630086de2ed3f64d4e9f622e3d8596b43e5
C. Serrano <serrano.cjm@gmail.com>
2020-09-15 22:53:36 -0400

    control commit

--- app/src/main/java/com/caparepa/companionfou/repository/nice/MaterialRepositoryImpl.kt
+++ app/src/main/java/com/caparepa/companionfou/repository/nice/MaterialRepositoryImpl.kt
@@ -1,18 +1,37 @@
 package com.caparepa.companionfou.repository.nice
 
+import com.caparepa.companionfou.data.db.dao.nice.MaterialDao
 import com.caparepa.companionfou.data.db.entity.nice.MaterialEntity
 import com.caparepa.companionfou.data.model.nice.material.MaterialItem
+import com.caparepa.companionfou.network.api.ApiClient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.koin.core.KoinComponent
 
-class MaterialRepositoryImpl : MaterialRepository {
+class MaterialRepositoryImpl(private val materialDao: MaterialDao) : MaterialRepository,
+    KoinComponent {
+
+    private val api = ApiClient.invoke()
+
     override suspend fun fetchMaterial(id: Long): MaterialEntity? {
-        TODO("Not yet implemented")
+        return materialDao.getMaterialById(id)
     }
 
     override suspend fun fetchMaterials(): List<MaterialEntity>? {
-        TODO("Not yet implemented")
+        return materialDao.getMaterials()
     }
 
-    override suspend fun getMaterialList(currentDate: String, region: String): List<MaterialItem>? {
-        TODO("Not yet implemented")
+    override suspend fun getMaterialList(currentDate: String, region: String): List<MaterialItem>? =
+        withContext(Dispatchers.IO) {
+            try {
+
+            } catch (e: Exception) {
+
+            }
+            TODO("Not yet implemented")
+        }
+
+    private suspend fun persistMaterialList(mList: List<MaterialItem>?) {
+
     }
 }
