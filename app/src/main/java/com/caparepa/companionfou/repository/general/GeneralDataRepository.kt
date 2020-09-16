4ef67788b809b99d6a2b69962b4b026790eaa0e5
C. Serrano <serrano.cjm@gmail.com>
2020-09-15 21:42:53 -0400

    misc

--- app/src/main/java/com/caparepa/companionfou/repository/general/GeneralDataRepository.kt
+++ app/src/main/java/com/caparepa/companionfou/repository/general/GeneralDataRepository.kt
@@ -3,13 +3,13 @@ package com.caparepa.companionfou.repository.general
 import okhttp3.ResponseBody
 
 interface GeneralDataRepository {
-    suspend fun getNiceAttributeRelation(urlType: String, region: String): ResponseBody?
-    suspend fun getNiceClassAttackRate(urlType: String, region: String): ResponseBody?
-    suspend fun getNiceClassRelation(urlType: String, region: String): ResponseBody?
-    suspend fun getNiceCard(urlType: String, region: String): ResponseBody?
-    suspend fun getNiceConstants(urlType: String, region: String): ResponseBody?
-    suspend fun getNiceBuffActionList(urlType: String, region: String): ResponseBody?
-    suspend fun getNiceUserLevel(urlType: String, region: String): ResponseBody?
+    suspend fun getAttributeRelation(urlType: String, region: String): ResponseBody?
+    suspend fun getClassAttackRate(urlType: String, region: String): ResponseBody?
+    suspend fun getClassRelation(urlType: String, region: String): ResponseBody?
+    suspend fun getFaceCard(urlType: String, region: String): ResponseBody?
+    suspend fun getConstants(urlType: String, region: String): ResponseBody?
+    suspend fun getBuffActionList(urlType: String, region: String): ResponseBody?
+    suspend fun getUserLevel(urlType: String, region: String): ResponseBody?
     suspend fun getAllEnums(urlType: String, region: String): ResponseBody?
     suspend fun getTraitMapping(urlType: String, region: String): ResponseBody?
 }
