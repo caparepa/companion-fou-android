8dc7f321c9dd1625db7cb03de273dfa80ec71870
C. Serrano <serrano.cjm@gmail.com>
2020-09-15 22:10:40 -0400

    misc

--- app/src/main/java/com/caparepa/companionfou/repository/general/GeneralDataRepository.kt
+++ app/src/main/java/com/caparepa/companionfou/repository/general/GeneralDataRepository.kt
@@ -1,15 +1,22 @@
 package com.caparepa.companionfou.repository.general
 
+import com.caparepa.companionfou.data.model.general.attackrate.ClassAttackRate
+import com.caparepa.companionfou.data.model.general.attribute.AttributeRelation
+import com.caparepa.companionfou.data.model.general.buffaction.BuffActionList
+import com.caparepa.companionfou.data.model.general.classrelation.ClassRelationList
+import com.caparepa.companionfou.data.model.general.facecards.FaceCardList
+import com.caparepa.companionfou.data.model.general.other.GameEnums
+import com.caparepa.companionfou.data.model.general.userlevel.UserLevelList
 import okhttp3.ResponseBody
 
 interface GeneralDataRepository {
-    suspend fun getAttributeRelation(urlType: String, region: String): ResponseBody?
-    suspend fun getClassAttackRate(urlType: String, region: String): ResponseBody?
-    suspend fun getClassRelation(urlType: String, region: String): ResponseBody?
-    suspend fun getFaceCard(urlType: String, region: String): ResponseBody?
+    suspend fun getAttributeRelation(urlType: String, region: String): AttributeRelation?
+    suspend fun getClassAttackRate(urlType: String, region: String): ClassAttackRate?
+    suspend fun getClassRelation(urlType: String, region: String): ClassRelationList?
+    suspend fun getFaceCard(urlType: String, region: String): FaceCardList?
     suspend fun getConstants(urlType: String, region: String): ResponseBody?
-    suspend fun getBuffActionList(urlType: String, region: String): ResponseBody?
-    suspend fun getUserLevel(urlType: String, region: String): ResponseBody?
-    suspend fun getAllEnums(urlType: String, region: String): ResponseBody?
+    suspend fun getBuffActionList(urlType: String, region: String): BuffActionList?
+    suspend fun getUserLevel(urlType: String, region: String): UserLevelList?
+    suspend fun getAllEnums(urlType: String, region: String): GameEnums?
     suspend fun getTraitMapping(urlType: String, region: String): ResponseBody?
 }
