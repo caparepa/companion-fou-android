bd824c49e131a9ee964852c29069ceae7c869797
C. Serrano <serrano.cjm@gmail.com>
2020-09-17 15:28:28 -0400

    control commit

--- app/src/main/java/com/caparepa/companionfou/data/db/entity/general/TraitEntity.kt
+++ app/src/main/java/com/caparepa/companionfou/data/db/entity/general/TraitEntity.kt
@@ -2,22 +2,16 @@ package com.caparepa.companionfou.data.db.entity.general
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
-import androidx.room.Index
 import androidx.room.PrimaryKey
 import com.caparepa.companionfou.utils.toKotlinObject
 
 @Entity(
-    tableName = "all_traits",
-    indices = [
-        Index(value = ["trait_id"], unique = true)
-    ]
+    tableName = "all_traits"
 )
 class TraitEntity(
     @ColumnInfo(name = "trait_map_value")
     val trait_map_value: String? = null
 ) {
-    //TODO: the original response must be processed from a Map<Int, String>
-
     @PrimaryKey(autoGenerate = true)
     var id: Long? = 0
 
