bcc9f0a642546f43d68e691490eb1fd49909ad89
C. Serrano <serrano.cjm@gmail.com>
2020-09-18 22:27:40 -0400

    implement command code repository

--- app/src/main/java/com/caparepa/companionfou/repository/nice/CommandCodeRepositoryImpl.kt
+++ app/src/main/java/com/caparepa/companionfou/repository/nice/CommandCodeRepositoryImpl.kt
@@ -1,9 +1,17 @@
 package com.caparepa.companionfou.repository.nice
 
+import com.caparepa.companionfou.data.db.dao.nice.CommandCodeDao
 import com.caparepa.companionfou.data.db.entity.nice.CommandCodeEntity
 import com.caparepa.companionfou.data.model.nice.commandcode.CommandCodeItem
+import com.caparepa.companionfou.network.api.ApiClient
+import com.caparepa.companionfou.utils.toJsonString
+import org.koin.core.KoinComponent
 
-class CommandCodeRepositoryImpl : CommandCodeRepository {
+class CommandCodeRepositoryImpl(private val commandCodeDao: CommandCodeDao) : CommandCodeRepository,
+    KoinComponent {
+
+    val api = ApiClient.invoke()
+
     override suspend fun fetchCommandCode(id: Long): CommandCodeEntity? {
         TODO("Not yet implemented")
     }
@@ -22,7 +30,16 @@ class CommandCodeRepositoryImpl : CommandCodeRepository {
     override suspend fun persistCommandCodeList(list: List<CommandCodeItem>?) {
         list?.let {
             it.forEach { item ->
-
+                val entity = CommandCodeEntity(
+                    item.id,
+                    item.collectionNo,
+                    item.name,
+                    item.rarity,
+                    item.extraAssets?.toJsonString(),
+                    item.skills?.toJsonString(),
+                    item.comment
+                )
+                commandCodeDao.upsert(entity)
             }
         }
     }
