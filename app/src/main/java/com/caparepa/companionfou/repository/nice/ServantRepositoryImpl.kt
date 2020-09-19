e54026f3c8554d8736190799909c2a710d6f7348
C. Serrano <serrano.cjm@gmail.com>
2020-09-18 23:14:09 -0400

    repository impl

--- app/src/main/java/com/caparepa/companionfou/repository/nice/ServantRepositoryImpl.kt
+++ app/src/main/java/com/caparepa/companionfou/repository/nice/ServantRepositoryImpl.kt
@@ -4,6 +4,7 @@ import com.caparepa.companionfou.data.db.dao.nice.ServantDao
 import com.caparepa.companionfou.data.db.entity.nice.ServantEntity
 import com.caparepa.companionfou.data.model.nice.servant.ServantItem
 import com.caparepa.companionfou.network.api.ApiClient
+import com.caparepa.companionfou.utils.toJsonString
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.koin.core.KoinComponent
@@ -23,7 +24,9 @@ class ServantRepositoryImpl(private val servantDao: ServantDao) : ServantReposit
     override suspend fun getServantList(currentDate: String, region: String): List<ServantItem>? =
         withContext(Dispatchers.IO) {
             try {
-                null
+                val response = api.getServantsWithLore(currentDate, region)
+                val body = response.body()
+                body
             } catch (e: Exception) {
                 e.printStackTrace()
                 null
@@ -31,6 +34,46 @@ class ServantRepositoryImpl(private val servantDao: ServantDao) : ServantReposit
         }
 
     override suspend fun persistServantList(list: List<ServantItem>?) {
-        TODO("Not yet implemented")
+        list?.let {
+            it.forEach { item ->
+                val entity = ServantEntity(
+                    item.id,
+                    item.collectionNo,
+                    item.name,
+                    item.className,
+                    item.type,
+                    item.rarity,
+                    item.cost,
+                    item.lvMax,
+                    item.extraAssets,
+                    item.gender,
+                    item.attribute,
+                    item.traits?.toJsonString(),
+                    item.starAbsorb,
+                    item.starGen,
+                    item.instantDeathChance,
+                    item.cards?.toJsonString(),
+                    item.hitsDistribution,
+                    item.atkBase,
+                    item.atkMax,
+                    item.hpBase,
+                    item.hpMax,
+                    item.growthCurve,
+                    item.atkGrowth?.toJsonString(),
+                    item.hpGrowth?.toJsonString(),
+                    item.bondGrowth?.toJsonString(),
+                    item.bondEquip,
+                    item.ascensionMaterials,
+                    item.skillMaterials,
+                    item.costumeMaterials,
+                    item.script,
+                    item.skills?.toJsonString(),
+                    item.classPassive?.toJsonString(),
+                    item.noblePhantasms?.toJsonString(),
+                    item.profile
+                )
+                servantDao.upsert(entity)
+            }
+        }
     }
 }
