779895b8d9731a2bfa86121dad3a65ced045c5b4
C. Serrano <serrano.cjm@gmail.com>
2020-10-10 09:42:43 -0400

    control commit

---
+++ app/src/main/java/com/caparepa/companionfou/ui/activity/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.caparepa.companionfou.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.caparepa.companionfou.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
