d236e12b8fefa29aee9e4b3a672b7a4d70b7ddbc
C. Serrano <serrano.cjm@gmail.com>
2020-10-11 11:46:47 -0400

    adapter scaffolding

---
+++ app/src/main/java/com/caparepa/companionfou/utils/adapters/equipment/CommandCodeAdapter.kt
@@ -0,0 +1,24 @@
+package com.caparepa.companionfou.utils.adapters.equipment
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class CommandCodeAdapter : RecyclerView.Adapter<CommandCodeAdapter.ViewHolder>() {
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/caparepa/companionfou/utils/adapters/materials/MysticCodeAdapter.kt
@@ -0,0 +1,24 @@
+package com.caparepa.companionfou.utils.adapters.materials
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MysticCodeAdapter : RecyclerView.Adapter<MysticCodeAdapter.ViewHolder>() {
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
