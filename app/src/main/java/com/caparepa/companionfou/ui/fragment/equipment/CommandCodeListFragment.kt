41a3e3da4ef83c8d5ef04a29c0a54d87c0e06af3
C. Serrano <serrano.cjm@gmail.com>
2020-10-10 22:15:05 -0400

    navigation graph (WIP)

--- app/src/main/java/com/caparepa/companionfou/ui/fragment/equipment/CommandCodeListFragment.kt
+++ app/src/main/java/com/caparepa/companionfou/ui/fragment/equipment/CommandCodeListFragment.kt
@@ -1,4 +1,4 @@
-package com.caparepa.companionfou.ui.fragment.materials
+package com.caparepa.companionfou.ui.fragment.equipment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [CommandCodeDetailFragment.newInstance] factory method to
+ * Use the [CommandCodeListFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CommandCodeDetailFragment : Fragment() {
+class CommandCodeListFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class CommandCodeDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_command_code_detail, container, false)
+        return inflater.inflate(R.layout.fragment_command_code_list, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class CommandCodeDetailFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment CommandCodeDetailFragment.
+         * @return A new instance of fragment CommandCodeListFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            CommandCodeDetailFragment().apply {
+            CommandCodeListFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
