8a4e61f411ef6bdaca53379a2ca3728971213927
C. Serrano <serrano.cjm@gmail.com>
2020-10-12 14:29:33 -0400

    adjust api endpoints

--- app/src/main/java/com/caparepa/companionfou/network/api/ExportDataEndpoints.kt
+++ app/src/main/java/com/caparepa/companionfou/network/api/ExportDataEndpoints.kt
@@ -22,62 +22,62 @@ interface ExportDataEndpoints {
 
     //Export endpoints
 
-    @GET("{current_date}/general/info.json")
+    @GET("{current_date}/info.json")
     suspend fun getApiInfo(
         @Path(value = "current_date", encoded = true) currentDate: String
     ): Response<ApiInfo>
 
     //Attribute Affinity
-    @GET("{current_date}/{region}/NiceAttributeRelation.json")
+    @GET("{current_date}/{region}/general/NiceAttributeRelation.json")
     suspend fun getAttributeRelation(
         @Path(value = "current_date", encoded = true) currentDate: String
     ): Response<AttributeRelation>
 
     //Attack Rate
-    @GET("{current_date}/{region}/NiceClassAttackRate.json")
+    @GET("{current_date}/{region}/general/NiceClassAttackRate.json")
     suspend fun getClassAttackRate(
         @Path(value = "current_date", encoded = true) currentDate: String
     ): Response<ClassAttackRate>
 
     //Class Affinity
-    @GET("{current_date}/{region}/NiceClassRelation.json")
+    @GET("{current_date}/{region}/general/NiceClassRelation.json")
     suspend fun getClassRelation(
         @Path(value = "current_date", encoded = true) currentDate: String
     ): Response<ClassRelationList>
 
     //Card Details
-    @GET("{current_date}/{region}/NiceCard.json")
+    @GET("{current_date}/{region}/general/NiceCard.json")
     suspend fun getFaceCard(
         @Path(value = "current_date", encoded = true) currentDate: String
     ): Response<FaceCardList>
 
     //Constants
-    @GET("{current_date}/{region}/NiceConstants.json")
+    @GET("{current_date}/{region}/general/NiceConstants.json")
     suspend fun getConstants(
         @Path(value = "current_date", encoded = true) currentDate: String
     ): Response<ResponseBody>
 
     //Buff Action Info
-    @GET("{current_date}/{region}/NiceBuffList.ActionList.json.json")
+    @GET("{current_date}/{region}/general/NiceBuffList.ActionList.json.json")
     suspend fun getBuffActionList(
         @Path(value = "current_date", encoded = true) currentDate: String
     ): Response<BuffActionList>
 
     //Master Level info
-    @GET("{current_date}/{region}/NiceUserLevel.json")
+    @GET("{current_date}/{region}/general/NiceUserLevel.json")
     suspend fun getUserLevel(
         @Path(value = "current_date", encoded = true) currentDate: String
     ): Response<Map<Int, UserLevelDetail>>
 
     //All enums
-    @GET("{current_date}/{region}/nice_enums.json")
+    @GET("{current_date}/{region}/general/nice_enums.json")
     suspend fun getAllEnums(
         @Path(value = "current_date", encoded = true) currentDate: String
     ): Response<GameEnums>
 
     //Trait mapping
     //TODO: the response is a Map<Int, String>?
-    @GET("{current_date}/{region}/nice_trait.json")
+    @GET("{current_date}/{region}/general/nice_trait.json")
     suspend fun getTraitMapping(
         @Path(value = "current_date", encoded = true) currentDate: String
     ): Response<Map<Int, String>?>
@@ -85,28 +85,28 @@ interface ExportDataEndpoints {
     //Region endpoints
 
     //Get servants
-    @GET("{current_date}/{region}/nice_servant.json")
+    @GET("{current_date}/{region}/nice/nice_servant.json")
     suspend fun getServants(
         @Path(value = "current_date", encoded = true) currentDate: String,
         @Path(value = "region", encoded = true) region: String
     ): Response<List<ServantItem>?>
 
     //Get servants with lore
-    @GET("{current_date}/{region}/nice_servant_lore.json")
+    @GET("{current_date}/{region}/nice/nice_servant_lore.json")
     suspend fun getServantsWithLore(
         @Path(value = "current_date", encoded = true) currentDate: String,
         @Path(value = "region", encoded = true) region: String
     ): Response<List<ServantItem>?>
 
     //Get Craft Essences
-    @GET("{current_date}/{region}/nice_equip.json")
+    @GET("{current_date}/{region}/nice/nice_equip.json")
     suspend fun getCraftEssences(
         @Path(value = "current_date", encoded = true) currentDate: String,
         @Path(value = "region", encoded = true) region: String
     ): Response<List<CraftEssenceItem>?>
 
     //Get Craft Essences with lore
-    @GET("{current_date}/{region}/nice_equip_lore.json")
+    @GET("{current_date}/{region}/nice/nice_equip_lore.json")
     suspend fun getCraftEssencesWithLore(
         @Path(
             value = "current_date",
@@ -115,21 +115,21 @@ interface ExportDataEndpoints {
     ): Response<List<CraftEssenceItem>?>
 
     //Get Command Codes
-    @GET("{current_date}/{region}/nice_command_code.json")
+    @GET("{current_date}/{region}/nice/nice_command_code.json")
     suspend fun getCommandCodes(
         @Path(value = "current_date", encoded = true) currentDate: String,
         @Path(value = "region", encoded = true) region: String
     ): Response<List<CommandCodeItem>?>
 
     //Get Materials (items, ascension, etc)
-    @GET("{current_date}/{region}/nice_item.json")
+    @GET("{current_date}/{region}/nice/nice_item.json")
     suspend fun getMaterials(
         @Path(value = "current_date", encoded = true) currentDate: String,
         @Path(value = "region", encoded = true) region: String
     ): Response<List<MaterialItem>?>
 
     //Get Mystic Codes
-    @GET("{current_date}/{region}/nice_mystic_code.json")
+    @GET("{current_date}/{region}/nice/nice_mystic_code.json")
     suspend fun getMysticCodes(
         @Path(value = "current_date", encoded = true) currentDate: String,
         @Path(value = "region", encoded = true) region: String
@@ -137,14 +137,14 @@ interface ExportDataEndpoints {
 
     //Basic data for indexing
     //Get servants
-    @GET("{current_date}/{region}/basic_servant.json")
+    @GET("{current_date}/{region}/basic/basic_servant.json")
     suspend fun getBasicServants(
         @Path(value = "current_date", encoded = true) currentDate: String,
         @Path(value = "region", encoded = true) region: String
     ): Response<ResponseBody>?
 
     //Get servants
-    @GET("{current_date}/{region}/basic_servant_lang_en.json")
+    @GET("{current_date}/{region}/basic/basic_servant_lang_en.json")
     suspend fun getBasicServantsEnglishName(
         @Path(
             value = "current_date",
@@ -153,20 +153,20 @@ interface ExportDataEndpoints {
     ): Response<ResponseBody>
 
     //Get Craft Essences
-    @GET("{current_date}/{region}/basic_equip.json")
+    @GET("{current_date}/{region}/basic/basic_equip.json")
     suspend fun getBasicCraftEssences(
         @Path(value = "current_date", encoded = true) currentDate: String,
         @Path(value = "region", encoded = true) region: String
     ): Response<ResponseBody>
 
-    @GET("{current_date}/{region}/basic_command_code.json")
+    @GET("{current_date}/{region}/basic/basic_command_code.json")
     suspend fun getBasicCommandCodes(
         @Path(value = "current_date", encoded = true) currentDate: String,
         @Path(value = "region", encoded = true) region: String
     ): Response<ResponseBody>
 
     //Get Mystic Codes
-    @GET("{current_date}/{region}/basic_mystic_code.json")
+    @GET("{current_date}/{region}/basic/basic_mystic_code.json")
     suspend fun getBasicMysticCodes(
         @Path(value = "current_date", encoded = true) currentDate: String,
         @Path(value = "region", encoded = true) region: String
