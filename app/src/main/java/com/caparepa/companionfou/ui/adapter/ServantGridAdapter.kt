e7c333cb91fe28a4dd9781ea23f026dc34269f12
C. Serrano <serrano.cjm@gmail.com>
2020-10-24 11:01:26 -0400

    servant grid adapter (wip)

--- app/src/main/java/com/caparepa/companionfou/ui/adapter/ServantGridAdapter.kt
+++ app/src/main/java/com/caparepa/companionfou/ui/adapter/ServantGridAdapter.kt
@@ -1,9 +1,13 @@
 package com.caparepa.companionfou.ui.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.caparepa.companionfou.R
 import com.caparepa.companionfou.data.db.entity.nice.ServantEntity
 
 class ServantGridAdapter(
@@ -14,22 +18,29 @@ class ServantGridAdapter(
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+        val servantGraph = itemView.findViewById<ImageView>(R.id.ivServantGraphBackground)
+        val classIcon = itemView.findViewById<ImageView>(R.id.ivClassIcon)
+        val rarityIcon = itemView.findViewById<ImageView>(R.id.ivRarityIcon)
+        val servantName = itemView.findViewById<TextView>(R.id.tvServantName)
 
-
-        fun bind() {
+        fun bind(servantItem: ServantEntity, onClick: (ServantEntity) -> Unit) {
 
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        val servantItemView = inflater.inflate(R.layout.item_servant_grid, parent, false)
+        return ViewHolder(servantItemView)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val servant: ServantEntity = servantList[position]
+        holder.bind(servant, onClick)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return servantList.size
     }
 }
