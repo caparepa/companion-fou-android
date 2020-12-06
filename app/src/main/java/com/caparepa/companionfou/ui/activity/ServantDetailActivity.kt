44969243844315583358a21b297809fac0e741c8
C. Serrano <serrano.cjm@gmail.com>
2020-12-06 11:54:04 -0400

    navigation (wip)

--- app/src/main/java/com/caparepa/companionfou/ui/activity/ServantDetailActivity.kt
+++ app/src/main/java/com/caparepa/companionfou/ui/activity/ServantDetailActivity.kt
@@ -16,6 +16,7 @@ class ServantDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_servant_detail)
+        initBottomNavigationView()
     }
 
     private fun initBottomNavigationView() {
